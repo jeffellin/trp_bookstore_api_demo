45c0c4143ee60e3c391773616c2cf7b316412ce9
Jeff <11721615+jeffellin@users.noreply.github.com>
2023-04-12 20:32:53 -0400

    add some title

--- src/main/java/com/vmware/example/bookstore/model/Book.java
+++ src/main/java/com/vmware/example/bookstore/model/Book.java
@@ -9,6 +9,21 @@ public class Book {
 @Id
 long id;
 
+String title;
+
+    public Book(long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public Book(long id) {
         this.id = id;
     }
