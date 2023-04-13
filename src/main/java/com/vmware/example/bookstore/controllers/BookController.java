80d676dca3e5a5c371817d5d5a7c978af553ac8b
Jeff <11721615+jeffellin@users.noreply.github.com>
2023-04-12 20:06:08 -0400

    add some docs

--- src/main/java/com/vmware/example/bookstore/controllers/BookController.java
+++ src/main/java/com/vmware/example/bookstore/controllers/BookController.java
@@ -2,6 +2,11 @@ package com.vmware.example.bookstore.controllers;
 
 import com.vmware.example.bookstore.model.Book;
 import com.vmware.example.bookstore.repositories.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +22,13 @@ public class BookController {
     @Autowired
     private BookRepository repository;
 
+    @Operation(summary = "Get a book by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "404", description = "Book not found",
+                    content = @Content) })
     @GetMapping("/{id}")
     public Book findById(@PathVariable long id) throws Throwable {
         return repository.findById(id)
